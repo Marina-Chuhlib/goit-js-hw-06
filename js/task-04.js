7e5ca9ec87e0cd936524997c161c0cfacd4805d3
Marina-Chuhlib <marinachuhlib@gmail.com>
2022-10-29 20:54:12 +0300

    done tasks 2-5

---
+++ js/task-04.js
@@ -0,0 +1,35 @@
+const decrementBtn = document.querySelector(`[data-action="decrement"]`);
+
+const incrementBtn = document.querySelector(`[data-action="increment"]`);
+
+const valueRef = document.querySelector("#value");
+
+let counterValue = 0;
+
+const handleClickDecrement = () => {
+  counterValue -= 1;
+
+  valueRef.textContent = counterValue;
+};
+
+const handleClickIncremen = () => {
+  counterValue += 1;
+
+  valueRef.textContent = counterValue;
+};
+
+decrementBtn.addEventListener("click", handleClickDecrement);
+
+incrementBtn.addEventListener("click", handleClickIncremen);
+
+// Счетчик состоит из спана и кнопок, которые, при клике, должны увеличивать и уменьшать его значение на единицу.
+
+// <div id="counter">
+//   <button type="button" data-action="decrement">-1</button>
+//   <span id="value">0</span>
+//   <button type="button" data-action="increment">+1</button>
+// </div>
+
+// Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
+// Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
+// Обновляй интерфейс новым значением переменной counterValue.
---
+++ js/task-05.js
@@ -0,0 +1,16 @@
+const inputRef = document.querySelector("#name-input");
+
+const output = document.querySelector("#name-output");
+
+inputRef.addEventListener("input", (event) => {
+  output.textContent =
+    event.currentTarget.value.trim() !== ""
+      ? event.currentTarget.value
+      : "Anonymous";
+});
+
+// Задание 5
+// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка "Anonymous".
+
+// <input type="text" id="name-input" placeholder="Please enter your name" />
+// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
